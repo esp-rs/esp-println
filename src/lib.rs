52f2a78e609c3fedd58a7502f62cd857c7b5c62c
dimi <dimi.polonski@gmail.com>
2023-03-28 16:50:17 +0200

    add `dbg!` macro

--- src/lib.rs
+++ src/lib.rs
@@ -51,6 +51,32 @@ macro_rules! print {
     ($($arg:tt)*) => {{}};
 }
 
+// implementation adapted from `std::dbg`
+#[macro_export]
+macro_rules! dbg {
+    // NOTE: We cannot use `concat!` to make a static string as a format argument
+    // of `eprintln!` because `file!` could contain a `{` or
+    // `$val` expression could be a block (`{ .. }`), in which case the `println!`
+    // will be malformed.
+    () => {
+        $crate::println!("[{}:{}]", ::core::file!(), ::core::line!())
+    };
+    ($val:expr $(,)?) => {
+        // Use of `match` here is intentional because it affects the lifetimes
+        // of temporaries - https://stackoverflow.com/a/48732525/1063961
+        match $val {
+            tmp => {
+                $crate::println!("[{}:{}] {} = {:#?}",
+                    ::core::file!(), ::core::line!(), ::core::stringify!($val), &tmp);
+                tmp
+            }
+        }
+    };
+    ($($val:expr),+ $(,)?) => {
+        ($($crate::dbg!($val)),+,)
+    };
+}
+
 pub struct Printer;
 
 impl core::fmt::Write for Printer {
